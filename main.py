eaa938c1e1644de00d5e294605b901e468cfbc96
Wolfmasternas <98648610+Wolfmasternas@users.noreply.github.com>
2022-07-21 16:25:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ async def load_start():
             print("Error came while clearing db")
             pass     
     await app.send_message(LOG_GROUP_ID, "Music Bot Started")
-    await client.send_message(LOG_GROUP_ID, "Assistant Of Zaid Music Started")
-    print("[INFO]: STARTED THE ZAID BOT AND SENDING THE INFO TO ZAID SERVER")
+    await client.send_message(LOG_GROUP_ID, "Assistant Of Shizuka Music Started")
+    print("[INFO]: STARTED THE Shizuka BOT AND SENDING THE INFO TO Shizuka SERVER")
     
    
 loop = asyncio.get_event_loop()
